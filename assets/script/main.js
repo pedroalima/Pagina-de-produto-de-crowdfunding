00fc298b0577b57f879e122b0d395c2ed22ff2b4
Pedro Lima <tsc.pedrolima@gmail.com>
2023-03-26 16:15:25 -0300

    feat adds dynamism to the acknowledgment modal

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,17 +5,17 @@ const menuCloseButton = document.querySelector("#close-menu")
 const fade = document.querySelector("#fade")
 const navbar = document.querySelector("#navbar")
 
-// Abrir menu mobile
+    // Abrir menu mobile
 menuOpenButton.addEventListener("click", () => {
     menuToggle()
 })
 
-// Fechar menu mobile
+    // Fechar menu mobile
 menuCloseButton.addEventListener("click", () => {
     menuToggle()
 })
 
-// Mostrar e esconder elementos
+    // Mostrar e esconder elementos
 function menuToggle() {
     menuMobile.classList.toggle("hide")
     fade.classList.toggle("hide")
@@ -27,16 +27,31 @@ const modalProductOpenButton = document.querySelector("#product__open-button")
 const modalProduct = document.querySelector("#modal-product")
 const modalProductCloseButton = document.querySelector("#product__close-button")
 
-// Abrir modal de produtos
+    // Abrir modal de produtos
 modalProductOpenButton.addEventListener("click", () => {
     modalProduct.showModal()
 })
 
-// Fechar modal de produtos
+    // Fechar modal de produtos
 modalProductCloseButton.addEventListener("click", () => {
     modalProduct.close()
 })
+
+// Constantes do modal de agradecimento
+const modalAcknowOpenButton = document.querySelector("#acknowledgment__open-button")
+const modalAcknow = document.querySelector("#modal-acknowledgment")
+const modalAcknowCloseButton = document.querySelector("#acknowledgment__close-button")
     
+    // Abrir modal de agradecimento
+modalAcknowOpenButton.addEventListener("click", () => {
+    modalAcknow.showModal()
+})
+
+    // Fechar modal de agradecimento
+modalAcknowCloseButton.addEventListener("click", () => {
+    modalAcknow.close()
+})
+
 /*
 const projectInput = document.querySelectorAll("input[type=radio]")
 */
